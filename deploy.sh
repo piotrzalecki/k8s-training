411dd4254fafc12ceb3d70f57dbf9875efbc40f6
Piotr Zalecki <zalecki@zalecki-mac.internal.synopsys.com>
2019-11-25 14:59:13 +0000

    On branch master  Changes to be committed:      modified:   .travis.yml      new file:   deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t piotrzalecki/multi-client:latest -t piotrzalecki/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t piotrzalecki/multi-server:latest -t piotrzalecki/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t piotrzalecki/multi-worker:latest -t piotrzalecki/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push piotrzalecki/multi-client:latest
+docker push piotrzalecki/multi-server:latest
+docker push piotrzalecki/multi-worker:latest
+
+docker push piotrzalecki/multi-client:$SHA
+docker push piotrzalecki/multi-server:$SHA
+docker push piotrzalecki/multi-worker:$SHA
+
+
+docker push piotrzalecki/multi-workerkubectl apply -f k8s
+kubectl set image deplomyents/server-deployment server=piotrzalecki/multi-client:$SHA
+kubectl set image deployments/client-deployment client=piotrzalecki/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=piotrzalecki/multi-worker
